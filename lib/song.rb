1731cd455550b9e2fd073304aaf6976f1b25b819
Joseph Corona <josed.hc96@gmail.com>
2018-11-18 21:40:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,16 @@ class Song
 
   @@count = 0
   @@artists = []
+  @@genres = []
 
   attr_accessor :name, :artist, :genre
 
   def initialize(_name, _artist, _genre)
     @name = _name
     @artist = _artist
+    @@artists << _artist
     @genre = _genre
+    @@genres << _genre
     @@count += 1
   end
 
