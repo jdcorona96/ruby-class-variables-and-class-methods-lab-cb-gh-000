ee4bdeb9b5765b68cdb23ef8a483df2bd568c4ca
Joseph Corona <josed.hc96@gmail.com>
2018-11-18 21:38:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,14 @@
 class Song
 
+  @@count = 0
+
   attr_accessor :name, :artist, :genre
 
   def initialize(_name, _artist, _genre)
     @name = _name
     @artist = _artist
     @genre = _genre
+    @@count += 1
   end
 
 end
